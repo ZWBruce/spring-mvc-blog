7da47212a443cbf2d0a63de27ddd0b4d8bc0edfb
张伟 <zhangwei.wzxzw@bytedance.com>
2023-10-14 11:44:44 +0800

    feat: 将图片存储路径改为 mac 上绝对路径

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,15 +1,23 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySource("file.properties")
 public class AppConfig implements WebMvcConfigurer {
+
+  @Value("${file.dir}")
+  String dir;
   
   @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      System.out.println("dir is: " + dir);
+
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("file:" + dir);
     }
 }
--- src/main/java/com/example/controller/FileController.java
+++ src/main/java/com/example/controller/FileController.java
@@ -7,15 +7,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @RestController
 @RequestMapping("/api")
+@PropertySource("file.properties")
 public class FileController {
+  @Value("${file.dir}")
+  String dir;
+
   @PostMapping("/file")
   public Resp<String> uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
     if(file.isEmpty()) {
@@ -24,9 +31,8 @@ public class FileController {
     String filename = file.getOriginalFilename();
 
     System.out.println("in file upload");
-    String rootDir = System.getProperty("user.dir");
 
-    Path filePath = Paths.get(rootDir).resolve("src/main/resources/static/" + filename);
+    Path filePath = Paths.get(this.dir).resolve(filename);
     
     
     // TODO 判断目录是否存在
@@ -41,7 +47,7 @@ public class FileController {
     int i = 1;
     while(Files.exists(filePath)) {
       finalFilename = name + "_" + (i++) + suffix;
-      filePath = Paths.get(rootDir).resolve("src/main/resources/static/" + finalFilename);
+      filePath = Paths.get(this.dir).resolve(finalFilename);
     }
     System.out.println("filePath: " + filePath.toString());
     Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
